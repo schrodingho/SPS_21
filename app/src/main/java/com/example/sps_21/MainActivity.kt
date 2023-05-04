b4b4199105aaf5f6bfec67043a0bba990fbf4480
schrodingho <15390486256@163.com>
2023-05-04 12:32:53 +0200

    new arch for recorder and player

--- app/src/main/java/com/example/sps_21/MainActivity.kt
+++ app/src/main/java/com/example/sps_21/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.sps_21
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,12 +7,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 import android.Manifest
-import android.content.pm.PackageManager
-import android.media.AudioFormat
-import android.media.AudioRecord
-import android.media.MediaRecorder
-import android.media.AudioTrack
-import android.media.AudioManager
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
@@ -23,8 +16,6 @@ import androidx.core.app.ActivityCompat
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.io.File
-import java.io.FileOutputStream
-import kotlin.experimental.and
 
 import androidx.compose.material.Text
 import androidx.compose.material.Button
@@ -33,28 +24,13 @@ import androidx.compose.material.SnackbarDuration
 import androidx.compose.material.rememberScaffoldState
 import com.example.sps_21.ui.theme.SPS_21Theme
 
-
 class MainActivity : ComponentActivity() {
-    private val SAMPLE_RATE = 44100
-    private val CHANNEL = AudioFormat.CHANNEL_IN_MONO;
-    private val ENCODING = AudioFormat.ENCODING_PCM_16BIT;
-    private val BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL, ENCODING)
-    private var outputStream: FileOutputStream? = null
-    private var recordingThread: Thread? = null
-    private var isRecording = false
-    private var recorder: AudioRecord? = null
-
-    // generate 20kHz tone
-    private val genFreq = 12000
-    private val PLAYER_CHANNEL = AudioFormat.CHANNEL_OUT_MONO;
-    private var TRACK_BUFFER_SIZE = 0
-    private val PLAY_DURATION = 3
-    private val numSamples = SAMPLE_RATE * PLAY_DURATION
-    private var samples = DoubleArray(numSamples)
-    private var gSnd = ByteArray(2 * numSamples)
-    private var playingThread: Thread? = null
-
-    private var player: AudioTrack? = null
+    val recorder by lazy {
+        Recorder(applicationContext)
+    }
+    val player by lazy {
+        Player(applicationContext)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,101 +40,13 @@ class MainActivity : ComponentActivity() {
             arrayOf(Manifest.permission.RECORD_AUDIO),
             0
         )
+
         setContent {
             SPS_21Theme {
                     ScaffoldDemo()
                 }
             }
         }
-    fun createRecorder() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.RECORD_AUDIO
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        recorder = AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE, CHANNEL, ENCODING, BUFFER_SIZE)
-        recorder?.startRecording()
-        isRecording = true
-
-        outputStream = FileOutputStream(cacheDir.absolutePath + "/recording12.pcm")
-
-        recordingThread = Thread( Runnable {
-            val buffer = ShortArray(BUFFER_SIZE)
-            while (isRecording) {
-                val read = recorder?.read(buffer, 0, BUFFER_SIZE) ?: 0
-                outputStream?.write(toByteArray(buffer), 0 , read * 2)
-            }
-        })
-        recordingThread?.start()
-    }
-
-    fun stopRecording() {
-        isRecording = false
-        recorder?.stop()
-        recorder?.release()
-        recorder = null
-        recordingThread = null
-        outputStream?.close()
-    }
-    private fun toByteArray(shortArray: ShortArray): ByteArray {
-        val byteArray = ByteArray(shortArray.size * 2)
-        for (i in shortArray.indices) {
-            byteArray[i * 2] = (shortArray[i] and 0xff).toByte()
-            byteArray[i * 2 + 1] = (shortArray[i].toInt() shr 8).toByte()
-        }
-        return byteArray
-    }
-
-    private fun createPlayer() {
-        try {
-            TRACK_BUFFER_SIZE = AudioTrack.getMinBufferSize(numSamples, PLAYER_CHANNEL, ENCODING)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        player = AudioTrack(
-            AudioManager.STREAM_MUSIC,
-            SAMPLE_RATE,
-            PLAYER_CHANNEL,
-            ENCODING,
-            TRACK_BUFFER_SIZE,
-            AudioTrack.MODE_STREAM
-        )
-        player?.write(gSnd, 0, gSnd.size)
-        player?.play()
-    }
-
-    private fun generateChirp() {
-        for (i in 0 until numSamples) {
-            samples[i] = Math.sin(2.0 * Math.PI * i.toDouble() / (SAMPLE_RATE / genFreq))
-        }
-
-        var idx = 0
-        for (dVal in samples) {
-            val shortVal = (dVal * 32767).toInt().toShort()
-            gSnd[idx++] = (shortVal and 0x00ff).toByte()
-            gSnd[idx++] = (shortVal and 0xff00.toShort()).toInt().ushr(8).toByte()
-        }
-    }
-
-    fun playChirp() {
-        playingThread = Thread(
-            Runnable {
-                generateChirp()
-                createPlayer()
-            }
-        )
-        playingThread?.start()
-    }
-
     fun generateSpectrum(){
         var spectrum = File(cacheDir.absolutePath,"spectrum.png")
         var pcm = File(cacheDir.absolutePath,"recording12.pcm")
@@ -186,14 +74,16 @@ fun MainActivity.ScaffoldDemo() {
                     scaffoldState.snackbarHostState.showSnackbar("Recording & Playing", duration = SnackbarDuration.Short)
                     delay(5000)
                 }
-                createRecorder()
-                playChirp()
+                File(cacheDir, "recording12.pcm").also {
+                    recorder.createRecorder(it)
+                    player.playChirp()
+                }
             }) {
                 Text(text = "Start Program")
             }
             Button(onClick = {
                 scope.launch { scaffoldState.snackbarHostState.showSnackbar("Recording stopped", duration = SnackbarDuration.Short) }
-                stopRecording()
+                recorder.stopRecording()
             }) {
                 Text(text = "Stop recording")
             }
---
+++ app/src/main/java/com/example/sps_21/Player.kt
@@ -0,0 +1,63 @@
+package com.example.sps_21
+
+import android.content.Context
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioTrack
+import kotlin.experimental.and
+
+class Player(applicationContext: Context) {
+    private val SAMPLE_RATE = 44100
+    private val ENCODING = AudioFormat.ENCODING_PCM_16BIT
+
+    private val genFreq = 12000
+    private val PLAYER_CHANNEL = AudioFormat.CHANNEL_OUT_MONO
+    private var TRACK_BUFFER_SIZE = 0
+    private val PLAY_DURATION = 3
+    private val numSamples = SAMPLE_RATE * PLAY_DURATION
+    private var samples = DoubleArray(numSamples)
+    private var gSnd = ByteArray(2 * numSamples)
+    private var playingThread: Thread? = null
+
+    private var player: AudioTrack? = null
+    fun createPlayer() {
+        try {
+            TRACK_BUFFER_SIZE = AudioTrack.getMinBufferSize(numSamples, PLAYER_CHANNEL, ENCODING)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        player = AudioTrack(
+            AudioManager.STREAM_MUSIC,
+            SAMPLE_RATE,
+            PLAYER_CHANNEL,
+            ENCODING,
+            TRACK_BUFFER_SIZE,
+            AudioTrack.MODE_STREAM
+        )
+        player?.write(gSnd, 0, gSnd.size)
+        player?.play()
+    }
+
+    fun generateChirp() {
+        for (i in 0 until numSamples) {
+            samples[i] = Math.sin(2.0 * Math.PI * i.toDouble() / (SAMPLE_RATE / genFreq))
+        }
+
+        var idx = 0
+        for (dVal in samples) {
+            val shortVal = (dVal * 32767).toInt().toShort()
+            gSnd[idx++] = (shortVal and 0x00ff).toByte()
+            gSnd[idx++] = (shortVal and 0xff00.toShort()).toInt().ushr(8).toByte()
+        }
+    }
+
+    fun playChirp() {
+        playingThread = Thread(
+            Runnable {
+                generateChirp()
+                createPlayer()
+            }
+        )
+        playingThread?.start()
+    }
+}
